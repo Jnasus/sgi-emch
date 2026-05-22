0f1e69304a14749971cc79ec9804b7d2c47ef1c2
Jnasus <jparionatorres@gmail.com>
2026-05-22 00:34:58 -0500

    feat: add allowed origins to SecurityConfig - add http://localhost:5173 to allowed origins - add https://sgi.escuelamilitar.edu.pe to allowed origins

--- backend/src/main/java/pe/edu/emch/sgi/config/SecurityConfig.java
+++ backend/src/main/java/pe/edu/emch/sgi/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://sgi.escuelamilitar.edu.pe"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
